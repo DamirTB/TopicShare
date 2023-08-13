c3ba7c0c04a44aee553eeec62019c6030cb2ef0f
DamirTB <talipdamir@gmail.com>
2023-08-13 19:45:35 +0600

    I have decided to separate forms from app.py into a new python file so it would be a much easier for me to work with application

---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, validators
+from wtforms.validators import DataRequired, InputRequired, Length
+
+class RegisterForm(FlaskForm):
+    username = StringField("", validators=[DataRequired()], render_kw={'placeholder': 'username', 'class' : 'form-control'})
+    password = PasswordField("", validators=[DataRequired()], render_kw={'placeholder': 'password', 'class' : 'form-control'})
+    password_repeat = PasswordField("", 
+                                    validators=[DataRequired(), 
+                                    validators.EqualTo('password', message='Password must match')], 
+                                    render_kw={'placeholder': 'repeat password', 'class' : 'form-control'})
+    submit = SubmitField("Enter", render_kw={'class' : 'btn btn-primary form-control '})
+
+class LoginForm(FlaskForm):
+    username = StringField("", validators=[DataRequired()], render_kw={'placeholder': 'username', 'class' : 'form-control'})
+    password = PasswordField("", validators=[DataRequired()], render_kw={'placeholder' : 'password', 'class' : 'form-control'})
+    submit = SubmitField("Enter", render_kw={'class' : 'btn btn-primary form-control '})
+
+class NoteForm(FlaskForm):
+    text = TextAreaField("Text", validators=[DataRequired()], render_kw={"rows": 4, "cols": 30, 'class' : 'form-control'})
+    submit = SubmitField("Submit", render_kw={'class' : 'btn btn-primary'})
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()], render_kw={'class' : 'form-control'})
+    text = TextAreaField("Text", validators=[DataRequired()], render_kw={"rows": 10, "cols": 30, 'class' : 'form-control'})
+    submit = SubmitField("Submit", render_kw={'class' : 'btn btn-primary'})
+
+
+
